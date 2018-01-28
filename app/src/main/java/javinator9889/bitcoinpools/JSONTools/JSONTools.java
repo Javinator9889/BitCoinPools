4a348f2705edbf3960c3910b7fc054cb2f3cb9ee
Javinator9889 <javialonso007@hotmail.es>
2018-01-29 00:26:42 +0100

    Initialized new LineChart (for BTC price). Finished with PoolsChart. Also created methods in JSONTools for sorting and retrieving information based on date (used coindesk API)

--- app/src/main/java/javinator9889/bitcoinpools/JSONTools/JSONTools.java
+++ app/src/main/java/javinator9889/bitcoinpools/JSONTools/JSONTools.java
@@ -1,16 +1,23 @@
 package javinator9889.bitcoinpools.JSONTools;
 
+import android.annotation.SuppressLint;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Javinator9889 on 20/12/2017.
@@ -50,5 +57,48 @@ public class JSONTools {
         } catch (JSONException e) {
             return null;
         }
+    }
+
+    public static HashMap<Date, Float> convert2DateHashMap(JSONObject object) {
+        HashMap<Date, Float> hReturn = new HashMap<>();
+        Iterator<String> iterator = object.keys();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                hReturn.put(dateFormat.parse(key), (float) object.getDouble(key));
+            }
+            return hReturn;
+        } catch (JSONException | ParseException e) {
+            return null;
+        }
+    }
+
+    public static Map<Date, Float> sortDateByValue(Map<Date, Float> unsortMap) {
+
+        /*// 1. Convert Map to List of Map
+        List<Map.Entry<Date, Float>> list =
+                new LinkedList<>(unsortMap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<Date, Float>>() {
+            public int compare(Map.Entry<Date, Float> o1, Map.Entry<Date, Float> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<Date, Float> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<Date, Float> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;*/
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Map<Date, Float> m1 = new TreeMap<>(unsortMap);
+        Map<Date, Float> returnMap = new LinkedHashMap<>();
+        for (Map.Entry<Date, Float> entry : m1.entrySet()) {
+            //System.out.println(df.format(entry.getKey()));
+            returnMap.put(entry.getKey(), entry.getValue());
+        }
+        return returnMap;
     }
 }
