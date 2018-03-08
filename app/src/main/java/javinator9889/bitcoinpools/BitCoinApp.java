747495fc57db8c930be86db6c7b52c58f838cc85
Javinator9889 <javialonso007@hotmail.es>
2018-03-08 18:50:09 +0100

    Improved BitCoinApp speed

--- app/src/main/java/javinator9889/bitcoinpools/BitCoinApp.java
+++ app/src/main/java/javinator9889/bitcoinpools/BitCoinApp.java
@@ -11,7 +11,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
@@ -41,7 +40,6 @@ public class BitCoinApp extends Application {
     @SuppressLint("StaticFieldLeak")
     private static Context APPLICATION_CONTEXT;
     private static SharedPreferences SHARED_PREFERENCES;
-    private static boolean isCacheCreated = false;
 
     public static Context getAppContext() {
         return APPLICATION_CONTEXT;
@@ -59,81 +57,95 @@ public class BitCoinApp extends Application {
                 Context.MODE_PRIVATE);
         initSharedPreferences();
         try {
-            isCacheCreated = CacheManaging.newInstance(this).setupFile();
-        } catch (IOException e) {
-            isCacheCreated = false;
-        }
+            CacheManaging.newInstance(this).setupFile();
+        } catch (IOException ignored) {} // This error should never happen
         startBackgroundJobs();
         super.onCreate();
         Log.d(Constants.LOG.BCTAG, Constants.LOG.CREATED_APP);
     }
 
     private static void startBackgroundJobs() {
-        JobScheduler mJobScheduler = (JobScheduler) APPLICATION_CONTEXT
-                .getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        JobInfo.Builder builder = new JobInfo.Builder(
-                1,
-                new ComponentName(APPLICATION_CONTEXT.getPackageName(),
-                        JobSchedulerService.class.getName()));
+        Thread backgroundJobsThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                JobScheduler mJobScheduler = (JobScheduler) APPLICATION_CONTEXT
+                        .getSystemService(Context.JOB_SCHEDULER_SERVICE);
+                JobInfo.Builder builder = new JobInfo.Builder(
+                        1,
+                        new ComponentName(APPLICATION_CONTEXT.getPackageName(),
+                                JobSchedulerService.class.getName()));
 
-        builder.setPeriodic(Constants.SCHEDULING_TIME);
-        builder.setPersisted(Constants.PERSISTED);
-        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-        builder.setBackoffCriteria(Constants.BACKOFF_CRITERIA, JobInfo.BACKOFF_POLICY_LINEAR);
+                builder.setPeriodic(Constants.SCHEDULING_TIME);
+                builder.setPersisted(Constants.PERSISTED);
+                builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+                builder.setBackoffCriteria(Constants.BACKOFF_CRITERIA,
+                        JobInfo.BACKOFF_POLICY_LINEAR);
 
-        assert mJobScheduler != null;
-        if (mJobScheduler.schedule(builder.build()) == JobScheduler.RESULT_FAILURE) {
-            Log.e(Constants.LOG.BCTAG,
-                    Constants.LOG.NO_INIT + "JobScheduler" + mJobScheduler.getAllPendingJobs()
-                            .toString());
-        }
+                assert mJobScheduler != null;
+                if (mJobScheduler.schedule(builder.build()) == JobScheduler.RESULT_FAILURE) {
+                    Log.e(Constants.LOG.BCTAG,
+                            Constants.LOG.NO_INIT + "JobScheduler"
+                                    + mJobScheduler.getAllPendingJobs().toString());
+                }
 
-        if (isJobCreationNeeded(mJobScheduler)) {
-            JobInfo.Builder cacheBuilder = new JobInfo.Builder(
-                    2,
-                    new ComponentName(APPLICATION_CONTEXT.getPackageName(),
-                            CacheJobSchedulerService.class.getName()));
+                if (isJobCreationNeeded(mJobScheduler)) {
+                    JobInfo.Builder cacheBuilder = new JobInfo.Builder(
+                            2,
+                            new ComponentName(APPLICATION_CONTEXT.getPackageName(),
+                                    CacheJobSchedulerService.class.getName()));
 
-            cacheBuilder.setPeriodic(TimeUnit.DAYS.toMillis(1));
-            cacheBuilder.setPersisted(Constants.PERSISTED);
-            cacheBuilder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-            cacheBuilder.setBackoffCriteria(Constants.BACKOFF_CRITERIA,
-                    JobInfo.BACKOFF_POLICY_LINEAR);
+                    cacheBuilder.setPeriodic(TimeUnit.DAYS.toMillis(1));
+                    cacheBuilder.setPersisted(Constants.PERSISTED);
+                    cacheBuilder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+                    cacheBuilder.setBackoffCriteria(Constants.BACKOFF_CRITERIA,
+                            JobInfo.BACKOFF_POLICY_LINEAR);
 
-            if (mJobScheduler.schedule(cacheBuilder.build()) == JobScheduler.RESULT_FAILURE) {
-                Log.e(Constants.LOG.BCTAG,
-                        Constants.LOG.NO_INIT + "JobScheduler" + mJobScheduler
-                                .getAllPendingJobs().toString());
-            } else {
-                SharedPreferences.Editor newValueForStartedJob = SHARED_PREFERENCES.edit();
-                newValueForStartedJob.putBoolean(Constants.SHARED_PREFERENCES.CACHE_JOB, true);
-                newValueForStartedJob.apply();
+                    if (mJobScheduler.schedule(cacheBuilder.build()) == JobScheduler
+                            .RESULT_FAILURE)
+                    {
+                        Log.e(Constants.LOG.BCTAG,
+                                Constants.LOG.NO_INIT + "JobScheduler" + mJobScheduler
+                                        .getAllPendingJobs().toString());
+                    } else {
+                        SharedPreferences.Editor newValueForStartedJob = SHARED_PREFERENCES.edit();
+                        newValueForStartedJob.putBoolean(Constants.SHARED_PREFERENCES.CACHE_JOB,
+                                true);
+                        newValueForStartedJob.apply();
+                    }
+                }
             }
-        }
+        });
+        backgroundJobsThread.start();
     }
 
     private void initSharedPreferences() {
-        if (!SHARED_PREFERENCES.contains(
-                Constants.SHARED_PREFERENCES.SHARED_PREFERENCES_INITIALIZED))
-        {
-            Log.d(Constants.LOG.BCTAG, Constants.LOG.INIT_PREF);
-            SharedPreferences.Editor sharedPreferencesEditor = SHARED_PREFERENCES.edit();
-            sharedPreferencesEditor.putBoolean(
-                    Constants.SHARED_PREFERENCES.SHARED_PREFERENCES_INITIALIZED, true);
-            sharedPreferencesEditor.putBoolean(
-                    Constants.SHARED_PREFERENCES.NOTIFICATIONS_ENABLED, false);
-            sharedPreferencesEditor.putBoolean(
-                    Constants.SHARED_PREFERENCES.NOTIFIED_LOW, false);
-            sharedPreferencesEditor.putBoolean(
-                    Constants.SHARED_PREFERENCES.NOTIFIED_HIGH, false);
-            sharedPreferencesEditor.putInt(
-                    Constants.SHARED_PREFERENCES.DAYS_TO_CHECK, 1);
-            sharedPreferencesEditor.putInt(
-                    Constants.SHARED_PREFERENCES.VALUE_TO_CHECK, 1000);
-            sharedPreferencesEditor.putString(
-                    Constants.SHARED_PREFERENCES.APP_VERSION, appVersion());
-            sharedPreferencesEditor.apply();
-        }
+        Thread sharedPreferencesThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (!SHARED_PREFERENCES.contains(
+                        Constants.SHARED_PREFERENCES.SHARED_PREFERENCES_INITIALIZED))
+                {
+                    Log.d(Constants.LOG.BCTAG, Constants.LOG.INIT_PREF);
+                    SharedPreferences.Editor sharedPreferencesEditor = SHARED_PREFERENCES.edit();
+                    sharedPreferencesEditor.putBoolean(
+                            Constants.SHARED_PREFERENCES.SHARED_PREFERENCES_INITIALIZED, true);
+                    sharedPreferencesEditor.putBoolean(
+                            Constants.SHARED_PREFERENCES.NOTIFICATIONS_ENABLED, false);
+                    sharedPreferencesEditor.putBoolean(
+                            Constants.SHARED_PREFERENCES.NOTIFIED_LOW, false);
+                    sharedPreferencesEditor.putBoolean(
+                            Constants.SHARED_PREFERENCES.NOTIFIED_HIGH, false);
+                    sharedPreferencesEditor.putInt(
+                            Constants.SHARED_PREFERENCES.DAYS_TO_CHECK, 1);
+                    sharedPreferencesEditor.putInt(
+                            Constants.SHARED_PREFERENCES.VALUE_TO_CHECK, 1000);
+                    sharedPreferencesEditor.putString(
+                            Constants.SHARED_PREFERENCES.APP_VERSION, appVersion());
+                    sharedPreferencesEditor.apply();
+                }
+            }
+        });
+        sharedPreferencesThread.start();
     }
 
     public static void forceRestartBackgroundJobs() {
