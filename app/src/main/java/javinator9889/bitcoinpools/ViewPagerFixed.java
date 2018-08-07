7973a8c519ad6fffa7bb31ca601ca3ed3c82765d
Javinator9889 <javialonso007@hotmail.es>
2018-08-07 19:24:45 +0200

    Release 1.18.6

---
+++ app/src/main/java/javinator9889/bitcoinpools/ViewPagerFixed.java
@@ -0,0 +1,41 @@
+package javinator9889.bitcoinpools;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ * Created by Javinator9889 on 07/08/2018.
+ * Based on: https://github.com/chrisbanes/PhotoView/issues/31
+ */
+public class ViewPagerFixed extends android.support.v4.view.ViewPager {
+    public ViewPagerFixed(Context context) {
+        super(context);
+    }
+
+    public ViewPagerFixed(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            Log.e("ViewPagerFX", "Error handling \"onTouchEvent\". Full trace: "
+                    + ex.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
